750fdd339777c38944dabc8dd16778bb7c63a58b
wojtek19022 <113895660+wojtek19022@users.noreply.github.com>
2025-07-25 19:57:06 +0200

    reorganization of a code

--- .docker/start.sh
+++ .docker/start.sh
@@ -4,7 +4,7 @@ docker compose -f docker-compose.yml up -d --force-recreate
 echo 'Wait 10 seconds'
 sleep 10
 echo 'Installation of the plugin'
-docker exec -t qgis sh -c "qgis_setup.sh wtyczka_qgis_app_2"
+docker exec -t qgis sh -c "qgis_setup.sh wtyczka_qgis_app"
 # docker exec -t qgis sh -c "pip3 install webdavclient3"
 docker exec -t qgis sh -c "pip3 install psycopg[binary]"
 echo 'Containers are running'
