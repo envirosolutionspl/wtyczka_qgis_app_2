1503329e7ecb090a54147037c7e33581a89a5ec5
Wojciech Sołyga <113895660+wojtek19022@users.noreply.github.com>
2025-08-06 15:25:40 +0200

    fixed partialy run for windows

--- wtyczka_qgis_app/test/test_save_layer_to_gml.py
+++ wtyczka_qgis_app/test/test_save_layer_to_gml.py
@@ -7,8 +7,8 @@ import pathlib
 import shutil
 import tempfile
 
-import processing
-from PyQt5.QtWidgets import QComboBox
+
+from qgis.PyQt.QtWidgets import QComboBox
 from qgis._core import QgsVectorLayer, QgsProject, QgsCoordinateReferenceSystem, QgsSettings
 
 # Allow running this test directly without relying on the test package.
@@ -94,83 +94,86 @@ class SaveLayerToGmlTest(unittest.TestCase):
             iface = DummyIface()
 
         plugin = AppModule(iface)
+        tmpdir = tempfile.TemporaryDirectory()
+        # with  as tmpdir:
+        s = QgsSettings()
+        s.setValue('wtyczka_qgis_app/settings/defaultPath', tmpdir)
+        s.setValue('wtyczka_qgis_app/settings/strefaPL2000', 2176)
+        s.setValue('wtyczka_qgis_app/settings/rodzajZbioru', 'POG')
+        s.setValue('wtyczka_qgis_app/settings/jpt', '321202')
 
-        with tempfile.TemporaryDirectory() as tmpdir:
-            s = QgsSettings()
-            s.setValue('qgis_app2/settings/defaultPath', tmpdir)
-            s.setValue('qgis_app2/settings/strefaPL2000', 2176)
-            s.setValue('qgis_app2/settings/rodzajZbioru', 'POG')
-            s.setValue('qgis_app2/settings/jpt', '321202')
+        # Copy GML fixtures to a writable directory so QGIS can create
+        # accompanying .gfs files without hitting permission errors.
+        app_gml = shutil.copy(self.app_gml, os.path.join(tmpdir, 'AktPlanowaniaPrzestrzennego.gml'))
+        spl_gml = shutil.copy(self.spl_gml, os.path.join(tmpdir, 'StrefaPlanistyczna.gml'))
 
-            # Copy GML fixtures to a writable directory so QGIS can create
-            # accompanying .gfs files without hitting permission errors.
-            app_gml = shutil.copy(self.app_gml, os.path.join(tmpdir, 'AktPlanowaniaPrzestrzennego.gml'))
-            spl_gml = shutil.copy(self.spl_gml, os.path.join(tmpdir, 'StrefaPlanistyczna.gml'))
-
-            gfs_source = os.path.join(self.plugin_dir, 'GFS', 'template.gfs')
-            gfs_target_dir = pathlib.Path(QgsApplication.qgisSettingsDirPath()) / 'python/plugins/wtyczka_qgis_app/GFS'
-            gfs_target_dir.mkdir(parents=True, exist_ok=True)
+        gfs_source = os.path.join(self.plugin_dir, 'GFS', 'template.gfs')
+        gfs_target_dir = pathlib.Path(QgsApplication.qgisSettingsDirPath()) / 'python/plugins/wtyczka_qgis_app/GFS'
+        if not os.path.exists(gfs_target_dir):
+            os.makedirs(gfs_target_dir, exist_ok=True)
             shutil.copyfile(gfs_source, gfs_target_dir / 'template.gfs')
 
-            templates_src = pathlib.Path(self.plugin_dir, 'modules', 'templates')
-            templates_dst = pathlib.Path(QgsApplication.qgisSettingsDirPath()) / 'python/plugins/wtyczka_qgis_app/modules/templates'
-            shutil.copytree(templates_src, templates_dst, dirs_exist_ok=True)
+        templates_src = pathlib.Path(self.plugin_dir, 'modules', 'templates')
+        templates_dst = pathlib.Path(QgsApplication.qgisSettingsDirPath()) / 'python/plugins/wtyczka_qgis_app/modules/templates'
+        if not os.path.exists(templates_dst):
+            shutil.copytree(templates_src, templates_dst, dirs_exist_ok=False)
 
-            # "/python/plugins/wtyczka_qgis_app/modules/app/A00_Granice_panstwa/A00_Granice_panstwa_bufor_300m.shp"
-            granice_src = pathlib.Path(self.plugin_dir, 'modules', 'app', 'A00_Granice_panstwa')
-            granice_dst = pathlib.Path(
-                QgsApplication.qgisSettingsDirPath()) / 'python/plugins/wtyczka_qgis_app/modules/app/A00_Granice_panstwa'
-            shutil.copytree(granice_src, granice_dst, dirs_exist_ok=True)
+        # "/python/plugins/wtyczka_qgis_app/modules/app/A00_Granice_panstwa/A00_Granice_panstwa_bufor_300m.shp"
+        granice_src = pathlib.Path(self.plugin_dir, 'modules', 'app', 'A00_Granice_panstwa')
+        granice_dst = pathlib.Path(
+            QgsApplication.qgisSettingsDirPath()) / 'python/plugins/wtyczka_qgis_app/modules/app/A00_Granice_panstwa'
+        if not os.path.exists(granice_dst):
+            shutil.copytree(granice_src, granice_dst, dirs_exist_ok=False)
 
-            app_layer = self.load_layer_from_file(app_gml)
+        app_layer = self.load_layer_from_file(app_gml)
 
-            # print(f'crs {app_layer.crs().authid()}')
-            # epsg = int(s.value('qgis_app2/settings/strefaPL2000'))
-            # if not app_layer.crs().authid():
-            #     app_layer.setCrs(QgsCoordinateReferenceSystem.fromEpsgId(epsg))
-            spl_layer = self.load_layer_from_file(spl_gml)
+        # print(f'crs {app_layer.crs().authid()}')
+        # epsg = int(s.value('qgis_app2/settings/strefaPL2000'))
+        # if not app_layer.crs().authid():
+        #     app_layer.setCrs(QgsCoordinateReferenceSystem.fromEpsgId(epsg))
+        spl_layer = self.load_layer_from_file(spl_gml)
 
-            self.assertTrue(app_layer.isValid(), 'AktPlanowaniaPrzestrzennego layer failed to load')
-            self.assertTrue(spl_layer.isValid(), 'SPL layer failed to load')
+        self.assertTrue(app_layer.isValid(), 'AktPlanowaniaPrzestrzennego layer failed to load')
+        self.assertTrue(spl_layer.isValid(), 'SPL layer failed to load')
 
-            # plugin.wektorInstrukcjaDialogPOG = type('dlg', (), {
-            #     'layers_comboBox': type('cmb', (), {'currentLayer': lambda self: app_layer})()
-            # })()
-            plugin.wektorInstrukcjaDialogPOG = type('dlg', (), {
-                'layers_comboBox': type('cmb', (), {'currentLayer': lambda self: app_layer})()
-            })()
-            plugin.activeDlg = plugin.wektorInstrukcjaDialogPOG
-            plugin.activeDlg.name = 'AktPlanowaniaPrzestrzennego'
-            plugin.activeDlg.layers_comboBox = QComboBox()
-            plugin.loadFromGMLorGPKG(False, app_gml)
-            out_path = os.path.join(tmpdir, 'output_pog.gml')
+        # plugin.wektorInstrukcjaDialogPOG = type('dlg', (), {
+        #     'layers_comboBox': type('cmb', (), {'currentLayer': lambda self: app_layer})()
+        # })()
+        plugin.wektorInstrukcjaDialogPOG = type('dlg', (), {
+            'layers_comboBox': type('cmb', (), {'currentLayer': lambda self: app_layer})()
+        })()
+        plugin.activeDlg = plugin.wektorInstrukcjaDialogPOG
+        plugin.activeDlg.name = 'AktPlanowaniaPrzestrzennego'
+        plugin.activeDlg.layers_comboBox = QComboBox()
+        plugin.loadFromGMLorGPKG(False, app_gml)
+        out_path = os.path.join(tmpdir, 'output_pog.gml')
 
-            from PyQt5 import QtWidgets
-            original_save = QtWidgets.QFileDialog.getSaveFileName
-            QtWidgets.QFileDialog.getSaveFileName = staticmethod(lambda directory=None, filter=None: (out_path, None))
-            try:
-                plugin.saveLayerToGML()
-            finally:
-                QtWidgets.QFileDialog.getSaveFileName = original_save
+        from qgis.PyQt import QtWidgets
+        original_save = QtWidgets.QFileDialog.getSaveFileName
+        QtWidgets.QFileDialog.getSaveFileName = staticmethod(lambda directory=None, filter=None: (out_path, None))
+        try:
+            plugin.saveLayerToGML()
+        finally:
+            QtWidgets.QFileDialog.getSaveFileName = original_save
 
-            self.assertTrue(os.path.exists(out_path), 'Output GML not created')
+        self.assertTrue(os.path.exists(out_path), 'Output GML not created')
 
-            plugin.wektorInstrukcjaDialogSPL = type('dlg', (), {
-                'layers_comboBox': type('cmb', (), {'currentLayer': lambda self: spl_layer})()
-            })()
-            plugin.activeDlg = plugin.wektorInstrukcjaDialogSPL
+        plugin.wektorInstrukcjaDialogSPL = type('dlg', (), {
+            'layers_comboBox': type('cmb', (), {'currentLayer': lambda self: spl_layer})()
+        })()
+        plugin.activeDlg = plugin.wektorInstrukcjaDialogSPL
 
-            out_path = os.path.join(tmpdir, 'output_spl.gml')
+        out_path = os.path.join(tmpdir, 'output_spl.gml')
 
-            from qgis.PyQt import QtWidgets
-            original_save = QtWidgets.QFileDialog.getSaveFileName
-            QtWidgets.QFileDialog.getSaveFileName = staticmethod(lambda directory=None, filter=None: (out_path, None))
-            try:
-                plugin.saveLayerToGML()
-            finally:
-                QtWidgets.QFileDialog.getSaveFileName = original_save
+        from qgis.PyQt import QtWidgets
+        original_save = QtWidgets.QFileDialog.getSaveFileName
+        QtWidgets.QFileDialog.getSaveFileName = staticmethod(lambda directory=None, filter=None: (out_path, None))
+        try:
+            plugin.saveLayerToGML()
+        finally:
+            QtWidgets.QFileDialog.getSaveFileName = original_save
 
-            self.assertTrue(os.path.exists(out_path), 'Output GML not created')
+        self.assertTrue(os.path.exists(out_path), 'Output GML not created')
 
 if __name__ == '__main__':
     unittest.main()
