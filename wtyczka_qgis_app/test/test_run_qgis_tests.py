151b17d048e3a25c5470777f88c4371fd519f5d1
Wojciech Sołyga <113895660+wojtek19022@users.noreply.github.com>
2025-08-12 11:52:40 +0200

    return previous test

--- wtyczka_qgis_app/test/test_run_qgis_tests.py
+++ wtyczka_qgis_app/test/test_run_qgis_tests.py
@@ -4,44 +4,24 @@
 # Copyright Lutra Consulting Limited
 
 
-# import sys
-# import unittest
-
-
-# def _run_tests(test_suite, package_name):
-#     count = test_suite.countTestCases()
-#     print("########")
-#     print("{} tests has been discovered in {}".format(count, package_name))
-#     print("########")
-
-#     unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(test_suite)
-
-
-# def test_all(package="."):
-#     test_loader = unittest.defaultTestLoader
-#     test_suite = test_loader.discover(package)
-#     _run_tests(test_suite, package)
-
-
-# if __name__ == "__main__":
-#     test_all()
-
-import importlib.util
-import pathlib
 import sys
 import unittest
-import os
 
-PLUGIN_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), os.pardir))# pathlib.Path(r"C:\Users\ms1\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins\wtyczka_qgis_app")
-TEST_PATH = os.path.join(PLUGIN_DIR, "test" , "test_save_layer_to_gml.py")
 
-# Ensure the plugin root is importable so test module can resolve internal imports
-if str(PLUGIN_DIR) not in sys.path:
-    sys.path.insert(0, str(PLUGIN_DIR))
+def _run_tests(test_suite, package_name):
+    count = test_suite.countTestCases()
+    print("########")
+    print("{} tests has been discovered in {}".format(count, package_name))
+    print("########")
 
-spec = importlib.util.spec_from_file_location("test_save_layer_to_gml", TEST_PATH)
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
+    unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(test_suite)
 
-suite = unittest.defaultTestLoader.loadTestsFromTestCase(module.SaveLayerToGmlTest)
-unittest.TextTestRunner(verbosity=2).run(suite)
+
+def test_all(package="."):
+    test_loader = unittest.defaultTestLoader
+    test_suite = test_loader.discover(package)
+    _run_tests(test_suite, package)
+
+
+if __name__ == "__main__":
+    test_all()
